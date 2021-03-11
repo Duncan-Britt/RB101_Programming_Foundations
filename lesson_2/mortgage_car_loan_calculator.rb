f4beadd4d805e79f8d8e5dcb3b5b3976b54d1c6d
Duncalicious <dbru997@gmail.com>
2021-03-10 17:46:20 -0700

    Rubocopped

--- lesson_2/mortgage_car_loan_calculator.rb
+++ lesson_2/mortgage_car_loan_calculator.rb
@@ -1,4 +1,9 @@
+require 'yaml'
+
+MESSAGES = YAML.load_file('./mortgage_messages.yml')
+
 def valid_number?(num)
+  return false if num.to_f < 0
   Float(num) rescue false
 end
 
@@ -12,38 +17,41 @@ def get_info(message)
   input
 end
 
-puts <<-MSG
-Welcome to the loan calculator.
-In order to calculate your monthly interest rate, your loan duration in months, and your monthly payments, we need some information from you. You'll be asked for the amount of the loan, the Annual Percentage Rate (APR) and the duratoin of the loan.
-
-MSG
-
-principle = ''
-
+puts MESSAGES['welcome']
 loop do
-  puts "Enter the loan amount:"
-  principle = gets.chomp
-  break if valid_number?(principle)
-  puts "Invalid input."
+  principle = ''
+
+  loop do
+    puts "Enter the loan amount:"
+    principle = gets.chomp
+    break if valid_number?(principle)
+    puts "Invalid input."
+  end
+
+  principle = principle.to_f
+
+  apr = get_info("Enter the percent APR:")
+
+  apr = apr.to_f / 100
+
+  duration = get_info("Enter the duration of the loan in years:")
+
+  duration = duration.to_f
+
+  monthly_ir = apr / 12
+  dur_months = duration * 12
+
+  monthly_payment = principle *
+                    (monthly_ir / (1 - (1 + monthly_ir)**(-dur_months)))
+
+  puts <<-MSG
+  Your monthly interest rate is #{monthly_ir * 100}%.
+  The duration in months is #{dur_months.to_i}.
+  The monthly payment is $#{format('%.2f', monthly_payment)}.
+  MSG
+
+  puts "Another calculation? (y)"
+  break unless gets.chomp.downcase.start_with?('y')
 end
 
-principle = principle.to_f
-
-apr = get_info("Enter the APR:")
-
-apr = apr.to_f/100
-
-duration = get_info("Enter the duration of the loan in years:")
-
-duration = duration.to_f
-
-monthly_ir = apr/12
-dur_months = duration * 12
-
-monthly_payment = principle * (monthly_ir / (1 - (1 + monthly_ir)**(-dur_months)))
-
-puts <<-MSG
-Your monthly interest rate is #{monthly_ir}.
-The duration in months is #{dur_months}.
-The monthly payment is #{monthly_payment}.
-MSG
+puts "Thanks and goodbye."
