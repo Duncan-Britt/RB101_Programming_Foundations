334b51afb4e1e2a792c9fe322f069f1782ecef91
Duncalicious <dbru997@gmail.com>
2021-03-23 21:00:40 -0600

    twenty_one.rb bonus features added

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -16,6 +16,9 @@ CARD_VALUES = {
   'Ace' => :A
 }
 
+MAX_VALUE = 21
+DEALER_MIN = 17
+
 def initialize_deck
   CARD_VALUES.keys * 4
 end
@@ -30,7 +33,7 @@ end
 
 def hand_value(non_ace_value, ace_count=0)
   return non_ace_value if ace_count.zero?
-  limit = 21 - non_ace_value
+  limit = MAX_VALUE - non_ace_value
   ace11_max = limit - 10
   non_ace_value + ace_value(ace_count, ace11_max)
 end
@@ -82,9 +85,11 @@ def print_cards(cards)
   result + ", and #{cards.shift}"
 end
 
-def display_turn(player_cards, dealer_cards)
+def display_turn(player_cards, dealer_cards, player_wins, dealer_wins)
   system 'clear'
-  puts "\n\n\n"
+  puts "\n"
+  puts "Player score: #{player_wins}. Dealer score: #{dealer_wins}"
+  puts "\n"
   puts "  Dealer has: #{dealer_cards[0]} and an unknown card"
   puts "  You have: #{print_cards(player_cards)}"
   puts "\n\n"
@@ -92,55 +97,78 @@ def display_turn(player_cards, dealer_cards)
   puts "\n"
 end
 
-def bust?(cards)
-  total_value(cards) > 21
+def bust?(value)
+  value > MAX_VALUE
 end
 
-def display_bust(cards)
+def display_bust(cards, player_total)
   system 'clear'
   puts "\n\n\n"
   puts "  You bust."
   puts "  Your hand: #{print_cards(cards)}"
-  puts "  Total value of #{total_value(cards)}"
+  puts "  Total value of #{player_total}"
   puts "\n\n"
 end
 
-def display_dealer_bust(dealer_cards)
+def display_dealer_bust(dealer_cards, dealer_total)
   system 'clear'
   puts "\n\n\n"
   puts "Dealer busted. You win!"
-  puts "His hand was: #{print_cards(dealer_cards)} for a total value of #{total_value(dealer_cards)}"
+  puts "His hand was: #{print_cards(dealer_cards)} for a total value of #{dealer_total}"
   puts "\n\n"
 end
 
-def winner(player_cards, dealer_cards)
-  if total_value(player_cards) > total_value(dealer_cards)
+def winner(player_total, dealer_total)
+  if player_total > dealer_total
     'You!'
-  elsif total_value(player_cards) < total_value(dealer_cards)
+  elsif player_total < dealer_total
     'the dealer.'
   else
     "No one. It's a tie"
   end
 end
 
-def declare_winner(player_cards, dealer_cards)
+def declare_winner(player_cards, dealer_cards, player_total, dealer_total)
   system 'clear'
   puts "\n\n\n"
-  puts "  Winner is: #{winner(player_cards, dealer_cards)}"
-  puts "  You had #{print_cards(player_cards)} for a total value of #{total_value(player_cards)}"
-  puts "  The dealer had #{print_cards(dealer_cards)} for a total value of #{total_value(dealer_cards)}"
+  puts "  Winner is: #{winner(player_total, dealer_total)}"
+  puts "  You had #{print_cards(player_cards)} for a total value of #{player_total}"
+  puts "  The dealer had #{print_cards(dealer_cards)} for a total value of #{dealer_total}"
   puts "\n\n"
 end
 
+def update_score(player_total, dealer_total)
+  if player_total > MAX_VALUE
+    return 0, 1
+  elsif dealer_total > MAX_VALUE
+    return 1, 0
+  elsif player_total > dealer_total
+    return 1, 0
+  elsif dealer_total > player_total
+    return 0, 1
+  else
+    return 0, 0
+  end
+end
+
+def display_grand_winner(winner)
+  puts "The grand winner is the #{winner}!"
+  puts "Thanks for playing."
+  sleep(10)
+end
+
 # GAME BEGINS
+player_wins = 0
+dealer_wins = 0
 loop do
   deck = initialize_deck
   player_cards, dealer_cards = initial_hand(deck)
 
   busted = false
   # PLAYER TURN
+  player_total = total_value(player_cards)
   loop do
-    display_turn(player_cards, dealer_cards)
+    display_turn(player_cards, dealer_cards, player_wins, dealer_wins)
     choice = ''
     loop do
       choice = gets.chomp.downcase
@@ -151,26 +179,43 @@ loop do
     end
     break if choice.start_with?('s')
     hit!(player_cards, deck)
-    if bust?(player_cards)
-      display_bust(player_cards)
+    player_total = total_value(player_cards)
+    if bust?(player_total)
+      dealer_wins += 1
+      display_bust(player_cards, player_total)
       busted = true
       break
     end
   end
 
   # DEALER TURN
+  dealer_total = total_value(dealer_cards)
   loop do
     break if busted
-    break if total_value(dealer_cards) >= 17
+    break if dealer_total >= DEALER_MIN
     hit!(dealer_cards, deck)
-    if bust?(dealer_cards)
-      display_dealer_bust(dealer_cards)
+    dealer_total = total_value(dealer_cards)
+    if bust?(dealer_total)
+      player_wins += 1
+      display_dealer_bust(dealer_cards, dealer_total)
       busted = true
     end
   end
 
-  declare_winner(player_cards, dealer_cards) unless busted
+  unless busted
+    declare_winner(player_cards, dealer_cards, player_total, dealer_total)
+    player_inc, dealer_inc = update_score(player_total, dealer_total)
+    player_wins += player_inc
+    dealer_wins += dealer_inc
+  end
 
+  if player_wins == 5
+    display_grand_winner('player')
+    break
+  elsif dealer_wins == 5
+    display_grand_winner('dealer')
+    break
+  end
   puts "Play again? (y)"
   input = gets.chomp.downcase
   break unless input.start_with?('y')
